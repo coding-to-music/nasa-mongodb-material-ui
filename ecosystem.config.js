3742b9866fae29d0623797bae3a4a5b4930ac9af
coding-to-music <connors.tom@gmail.com>
2022-03-27 02:28:50 +0000

    package

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,5 +12,15 @@ module.exports = {
       watch: false,
       max_memory_restart: "1G",
     },
+    {
+      name: "Client",
+      script: "npm prod",
+
+      // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "1G",
+    },
   ],
 };
